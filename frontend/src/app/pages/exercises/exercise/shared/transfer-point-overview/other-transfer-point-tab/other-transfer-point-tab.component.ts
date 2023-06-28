ee59921d20e549c003374035cecba214d1743857
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-06-28 15:29:00 +0200

    Add keyboard navigation for transfer connections

--- frontend/src/app/pages/exercises/exercise/shared/transfer-point-overview/other-transfer-point-tab/other-transfer-point-tab.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/transfer-point-overview/other-transfer-point-tab/other-transfer-point-tab.component.ts
@@ -1,10 +1,17 @@
-import type { OnInit } from '@angular/core';
-import { Component, Input } from '@angular/core';
+import type { OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 import { Store } from '@ngrx/store';
 import { UUID, TransferPoint } from 'digital-fuesim-manv-shared';
 import type { Observable } from 'rxjs';
 import { combineLatest, map } from 'rxjs';
 import { ExerciseService } from 'src/app/core/exercise.service';
+import type { SearchableDropdownOption } from 'src/app/shared/components/searchable-dropdown/searchable-dropdown.component';
+import type { HotkeyLayer } from 'src/app/shared/services/hotkeys.service';
+import {
+    Hotkey,
+    HotkeysService,
+} from 'src/app/shared/services/hotkeys.service';
 import type { AppState } from 'src/app/state/app.state';
 import {
     createSelectTransferPoint,
@@ -16,8 +23,10 @@ import {
     templateUrl: './other-transfer-point-tab.component.html',
     styleUrls: ['./other-transfer-point-tab.component.scss'],
 })
-export class OtherTransferPointTabComponent implements OnInit {
+export class OtherTransferPointTabComponent implements OnInit, OnDestroy {
     @Input() public transferPointId!: UUID;
+
+    @ViewChild(NgbPopover) popover!: NgbPopover;
 
     public transferPoint$!: Observable<TransferPoint>;
 
@@ -28,13 +37,17 @@ export class OtherTransferPointTabComponent implements OnInit {
     /**
      * All transferPoints that are neither connected to this one nor this one itself
      */
-    public transferPointsToBeAdded$!: Observable<{
-        [key: UUID]: TransferPoint;
-    }>;
+    public transferPointsToBeAdded$!: Observable<SearchableDropdownOption[]>;
+
+    private hotkeyLayer!: HotkeyLayer;
+    public addConnectionHotkey = new Hotkey('+', false, (event) => {
+        this.popover.open();
+    });
 
     constructor(
         private readonly store: Store<AppState>,
-        private readonly exerciseService: ExerciseService
+        private readonly exerciseService: ExerciseService,
+        private readonly hotkeysService: HotkeysService
     ) {}
 
     ngOnInit() {
@@ -48,13 +61,16 @@ export class OtherTransferPointTabComponent implements OnInit {
             map((transferPoints) => {
                 const currentTransferPoint =
                     transferPoints[this.transferPointId]!;
-                return Object.fromEntries(
-                    Object.entries(transferPoints).filter(
+                return Object.entries(transferPoints)
+                    .filter(
                         ([key]) =>
                             key !== this.transferPointId &&
                             !currentTransferPoint.reachableTransferPoints[key]
                     )
-                );
+                    .map(([id, transferPoint]) => ({
+                        identifier: id,
+                        name: TransferPoint.getFullName(transferPoint),
+                    }));
             })
         );
 
@@ -72,6 +88,13 @@ export class OtherTransferPointTabComponent implements OnInit {
                     .sort((a, b) => a.name.localeCompare(b.name))
             )
         );
+
+        this.hotkeyLayer = this.hotkeysService.createLayer();
+        this.hotkeyLayer.addHotkey(this.addConnectionHotkey);
+    }
+
+    ngOnDestroy() {
+        this.hotkeysService.removeLayer(this.hotkeyLayer);
     }
 
     public connectTransferPoint(transferPointId: UUID, duration?: number) {
